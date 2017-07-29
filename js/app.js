61b4da1c159ebf62865d38e66552d4d065ade8b1
Idmar Ramos Junior (Id) <idmar.ramos@booking.com>
2017-07-29 17:22:44 +0200

    Add app.js

---
+++ js/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	///////////////////////////////////////////////////////////////////////////
+	// Animations
+	///////////////////////////////////////////////////////////////////////////
+	AOS.init();
+
+	///////////////////////////////////////////////////////////////////////////
+	// Old browser upgrade message
+	///////////////////////////////////////////////////////////////////////////
+	//event listener: DOM ready
+	function addLoadEvent(func) {
+		var oldonload = window.onload;
+		if (typeof window.onload != 'function') {
+			window.onload = func;
+		} else {
+			window.onload = function() {
+				if (oldonload) {
+					oldonload();
+				}
+				func();
+			}
+		}
+	}
+	//call plugin function after DOM ready
+	addLoadEvent(function(){
+		outdatedBrowser({
+			bgColor: '#f25648',
+			color: '#ffffff',
+			lowerThan: 'transform',
+			languagePath: '../bower_components/outdated-browser/outdatedbrowser/lang/pt-br.html'
+		})
+	});
+
+//End document ready
+});
